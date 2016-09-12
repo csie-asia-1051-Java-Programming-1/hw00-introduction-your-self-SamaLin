17bdc2fd5cac386090b878b25d74efe27778bd6d
SamaLin <105021058@live.asia.edu.tw>
2016-09-12 15:51:22 +0800

    hw00 homework submit

---
+++ hw00/src/SamaLin.java
@@ -0,0 +1,12 @@
+//import j
+public class SamaLin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello!");
+		System.out.println("my name is 蕭懋霖");
+		System.out.println("105021058");
+		System.out.println("亞洲大學資工系數位組");
+	}
+
+}
